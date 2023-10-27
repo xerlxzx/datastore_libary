76527b2573c12e33229db8da6c3d66459ba23f4e
xerlxzx <108105936+xerlxzx@users.noreply.github.com>
2023-10-27 23:58:00 +1100

    Update Database_intereactions.lua

--- Database_intereactions.lua
+++ Database_intereactions.lua
@@ -1,64 +1,63 @@
-local function Get_Value_Type(value)
-    local valuetypes_content = { "number", "string", "boolean", "table", "nill", "function", "thread", "Instance" }
-
-    local tablesize = 6
-
-    for v = 0, tablesize do
-        if type(value) == valuetypes_content[v] then
-            return valuetypes_content[v]
-        else
-            error("Data Type Not available")
-        end
-    end
-end
-
-
 --[[
 local DataStoreService = game:GetService("DataStoreService")
-local dataStore = DataStoreService:GetDataStore("Data_Store_Test_v0")
+local dataStore_test0 = DataStoreService:GetDataStore("Data_Store_Test_v0")
 ]]
+-- to get datastore
 
+
+local function Get_Value_Type(value)
+	local valuetypes_content = { "number", "string", "boolean", "table", "nill", "function", "thread", "Instance" }
+
+	local tablesize = 6
+
+	for v = 0, tablesize do
+		if type(value) == valuetypes_content[v] then
+			return valuetypes_content[v]
+		else
+			error("Data Type Not available")
+		end
+	end
+end
 
 local function Has_player_joined(playerinst, UserId, datastore)
-    local data = datastore:GetAsync(UserId)
-    if data then
-        local valueholder = data["Has_Joined_BF"]
-        if valueholder == true then
-            return true
-        else
-            return false
-        end
-    else
-        return false
-    end
+	local data = datastore:GetAsync(UserId)
+	if data then
+		local valueholder = data["Has_Joined_BF"]
+		if valueholder == true then
+			return true
+		else
+			return false
+		end
+	else
+		return false
+	end
 end
 
 local function Write_Data(playerinst, UserId, valuetable, datastore)
-    local success, errorcode = pcall(function()
-        datastore:SetAsync(UserId, valuetable)
-    end)
+	local success, errorcode = pcall(function()
+		datastore:SetAsync(UserId, valuetable)
+	end)
 
-    if success then
-        print("Data write success")
-    else
-        print("Data failed to write")
-        error(errorcode)
-    end
+	if success then
+		print("Data write success")
+	else
+		print("Data failed to write")
+		error(errorcode)
+	end
 end
 
-
 local function Get_Data(playerinst, UserId, dataStore)
-    local data
+	local data
 
-    local success, errorcode = pcall(function()
-        data = dataStore:GetAsync(UserId)
-    end)
+	local success, errorcode = pcall(function()
+		data = dataStore:GetAsync(UserId)
+	end)
 
-    if success and data then
-        print("data retrieved")
-        return data
-    else
-        print("Failed to fetch Data")
-        error(errorcode)
-    end
+	if success and data then
+		print("data retrieved")
+		return data
+	else
+		print("Failed to fetch Data")
+		error(errorcode)
+	end
 end
